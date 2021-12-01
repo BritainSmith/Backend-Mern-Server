fdc60a324b21e6bdfd652372fab18458fd117d80
Britain Smith <Britain.D.Smith@gmail.com>
2021-12-01 10:35:01 -0600

    create like/dislikes routes, get routes, and get timeline routes for posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 /** @format */
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 const router = require("express").Router();
 
@@ -53,8 +54,55 @@ router.delete("/:id", async (req, res) => {
 	}
 });
 
-//Like a Post
+//Like / dislike a Post
+
+router.put("/:id/like", async (req, res) => {
+	try {
+		// Find the post
+		const post = await Post.findById(req.params.id);
+		// When post is found, check if it's liked by the user and push the userId to the likes array.
+		if (!post.likes.includes(req.body.userId)) {
+			await post.updateOne({ $push: { likes: req.body.userId } });
+			res.status(200).json("The post has been liked!");
+		} else {
+			//else if the post is disliked, pull the user from the likes array.
+			await post.updateOne({ $pull: { likes: req.body.userId } });
+			res.status(200).json("The post has been disliked.");
+		}
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 //Get a Post
+router.get("/:id", async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		res.status(200).json(post);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 //Get timeline Posts (Get all posts in user's following array)
+router.get("/timeline/all", async (req, res) => {
+	try {
+		// find the current user
+		const currentUser = await User.findById(req.body.userId);
+		// find the current users posts
+		const userPosts = await Post.find({ userId: currentUser._id });
+		// find their friends posts by matching user ids that are the user's friends (friendId)
+		//by accessing the following array.
+		// Then map over the following away to determine your friends posts, and bind them with the user's posts
+		const friendPosts = await Promise.all(
+			currentUser.following.map((friendId) => {
+				return Post.find({ userId: friendId });
+			})
+		);
+		res.status(200).json(userPosts.concat(...friendPosts));
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
 module.exports = router;
